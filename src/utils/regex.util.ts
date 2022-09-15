73b47a90382c983ba72a82de91759dcab5c4ae0f
jcamilofarfan <farfan1976@hotmail.com>
2022-09-15 12:53:23 -0500

    company is changed by username

--- src/utils/regex.util.ts
+++ src/utils/regex.util.ts
@@ -1,6 +1,6 @@
 const email= /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 const password= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
-const compania= /^[a-zA-Z0-9]+([_\s\-]?[a-zA-Z0-9])*$/
+const username= /^[a-zA-Z0-9]+([_\s\-]?[a-zA-Z0-9])*$/
 const expDefault = /^[0-9]/
 export const validateInput = (value: string, field: string) => {
     let expresion;
@@ -11,8 +11,8 @@ export const validateInput = (value: string, field: string) => {
         case 'password':
             expresion = password
             break
-        case 'compania':
-            expresion = compania
+        case 'username':
+            expresion = username
             break
         default:
             expresion = expDefault
@@ -30,8 +30,8 @@ export const getRegex = (field:string) => {
         case 'password':
             expresion = password
             break
-        case 'compania':
-            expresion = compania
+        case 'username':
+            expresion = username
             break
         default:
             expresion = expDefault
